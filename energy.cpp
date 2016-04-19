eb3839f5deb8bd50b1d578b7864a273042e6c496
juchenaolo <juanhenao21@gmail.com>
2016-04-19 14:15:36 -0500

    Testing something with the branches

--- energy.cpp
+++ energy.cpp
@@ -13,12 +13,12 @@ std::vector<Atom> atoms;
  * @param atoms  vector with the spin values of the atom
  * @param spin  random spin orientation of the atom
  * @param csr  compressed sparse row that contains the information of the atoms' neighbors
- * @return double Ener the energy of the system.
+ * @return double ener the energy of the system.
  */
 double compute_energy (const std::vector<Atom>& atoms, const std::vector<Spin>& spins, const CSRMatrix& csr)
 {
     /** Energy of the system. */
-    double Ener{0};
+    double ener{0};
     /** Spin value of the atom and the neighbors. */
     double SpinValue;
 
@@ -27,9 +27,9 @@ double compute_energy (const std::vector<Atom>& atoms, const std::vector<Spin>& 
         for (int j = csr.limits[i] ; j < csr.limits[i+1]; ++j)
         {
             SpinValue = spins[i] * spins[csr.neighboors[j]];
-            Ener -= csr.exchanges[j] * atoms[i].s * atoms[csr.neighboors[j]].s * SpinValue;
+            ener -= csr.exchanges[j] * atoms[i].s * atoms[csr.neighboors[j]].s * SpinValue;
         }
     }
 
-    return Ener;
+    return ener;
 }
